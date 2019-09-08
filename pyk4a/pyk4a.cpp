27dfa6ee7a078042487124307f0d9fa4f4e446e1
Etienne Dubeau <etienne.dubeau.1@ulaval.ca>
2019-09-08 10:05:30 -0400

    Add connect/close methods

--- pyk4a/pyk4a.cpp
+++ pyk4a/pyk4a.cpp
@@ -3,92 +3,108 @@
 #include <k4a/k4a.h>
 #include <stdio.h>
 
-static PyObject* hello_world(PyObject* self, PyObject* args)
-{
-    uint32_t device_count = 0;
-    k4a_device_t device = NULL;
-    k4a_device_configuration_t config = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-    device_count = k4a_device_get_installed_count();
+    static PyObject* device_open(PyObject* self, PyObject* args){
+        int device_id;
+        PyArg_ParseTuple(args, "I", &device_id);
+        k4a_device_t device = NULL;
+        k4a_result_t result = k4a_device_open(device_id, &device);
 
-    if (device_count == 0)
-    {
-        printf("No K4A devices found\n");
-        return Py_BuildValue("I", 1);
+        return Py_BuildValue("In", result, device);
     }
 
-    if (K4A_RESULT_SUCCEEDED != k4a_device_open(K4A_DEVICE_DEFAULT, &device))
-    {
-        printf("Failed to open device\n");
-        return Py_BuildValue("I", 1);
+    static PyObject* device_close(PyObject* self, PyObject* args){
+        k4a_device_t device = NULL;
+        PyArg_ParseTuple(args, "n", &device);
+
+        /* k4a_device_close(device); */
+        return NULL;
     }
 
-    return Py_BuildValue("I", 0);
-}
+    static PyObject* device_get_sync_jack(PyObject* self, PyObject* args){
+        k4a_device_t device = NULL;
+        PyArg_ParseTuple(args, "n", &device);
 
-// Source : https://github.com/MathGaron/pyvicon/blob/master/pyvicon/pyvicon.cpp
-//###################
-//Module initialisation
-//###################
+        bool in_jack = 0;
+        bool out_jack = 0;
+        k4a_result_t result = k4a_device_get_sync_jack(device, &in_jack, &out_jack);
 
-struct module_state
-{
-    PyObject *error;
-};
+        return Py_BuildValue("III", result, in_jack, out_jack);
+    }
+
+
+    // Source : https://github.com/MathGaron/pyvicon/blob/master/pyvicon/pyvicon.cpp
+    //###################
+    //Module initialisation
+    //###################
+
+    struct module_state
+    {
+        PyObject *error;
+    };
 
 #define GETSTATE(m) ((struct module_state*)PyModule_GetState(m))
 
 
-//#####################
-// Methods
-//#####################
-static PyMethodDef Pyk4aMethods[] = {
-     {"hello_world", hello_world, METH_VARARGS, "Hello world"},
-     {NULL, NULL, 0, NULL}
-};
+    //#####################
+    // Methods
+    //#####################
+    static PyMethodDef Pyk4aMethods[] = {
+        {"device_open", device_open, METH_VARARGS, "Open an Azure Kinect device"},
+        {"device_close", device_close, METH_VARARGS, "Close an Azure Kinect device"},
+        {"device_get_sync_jack", device_get_sync_jack, METH_VARARGS, "Get the device jack status for the synchronization in and synchronization out connectors."},
+        {NULL, NULL, 0, NULL}
+    };
 
-static int pyk4a_traverse(PyObject *m, visitproc visit, void *arg)
-{
-    Py_VISIT(GETSTATE(m)->error);
-    return 0;
-}
+    static int pyk4a_traverse(PyObject *m, visitproc visit, void *arg)
+    {
+        Py_VISIT(GETSTATE(m)->error);
+        return 0;
+    }
 
-static int pyk4a_clear(PyObject *m)
-{
-    Py_CLEAR(GETSTATE(m)->error);
-    return 0;
-}
+    static int pyk4a_clear(PyObject *m)
+    {
+        Py_CLEAR(GETSTATE(m)->error);
+        return 0;
+    }
 
-static struct PyModuleDef moduledef =
-{
-    PyModuleDef_HEAD_INIT,
-    "k4a_module",
-    NULL,
-    sizeof(struct module_state),
-    Pyk4aMethods,
-    NULL,
-    pyk4a_traverse,
-    pyk4a_clear,
-    NULL
-};
+    static struct PyModuleDef moduledef =
+    {
+        PyModuleDef_HEAD_INIT,
+        "k4a_module",
+        NULL,
+        sizeof(struct module_state),
+        Pyk4aMethods,
+        NULL,
+        pyk4a_traverse,
+        pyk4a_clear,
+        NULL
+    };
 #define INITERROR return NULL
 
 
-//########################
-// Module init function
-//########################
-PyMODINIT_FUNC PyInit_k4a_module(void) {
-    PyObject *module = PyModule_Create(&moduledef);
+    //########################
+    // Module init function
+    //########################
+    PyMODINIT_FUNC PyInit_k4a_module(void) {
+        PyObject *module = PyModule_Create(&moduledef);
 
-    if (module == NULL)
-    INITERROR;
-    struct module_state *st = GETSTATE(module);
+        if (module == NULL)
+            INITERROR;
+        struct module_state *st = GETSTATE(module);
 
-    st->error = PyErr_NewException("pyvicon_module.Error", NULL, NULL);
-    if (st->error == NULL)
-    {
-        Py_DECREF(module);
-        INITERROR;
+        st->error = PyErr_NewException("pyvicon_module.Error", NULL, NULL);
+        if (st->error == NULL)
+        {
+            Py_DECREF(module);
+            INITERROR;
+        }
+        return module;
     }
-return module;
+
+#ifdef __cplusplus
 }
+#endif
--- pyk4a/pyk4a.py
+++ pyk4a/pyk4a.py
@@ -1,3 +1,31 @@
 import k4a_module
+from enum import Enum
 
-print(k4a_module.hello_world())
+# k4a_result_t
+class Result(Enum):
+    Success = 0
+    Failed = 1
+
+class PyK4A:
+    def device_open(self, device_id=0):
+        res, self._device = k4a_module.device_open(device_id)
+        self._verify_error(res)
+
+    def device_close(self):
+        k4a_module.device_close(self._device)
+
+    def device_get_sync_jack(self):
+        res, jack_in, jack_out = k4a_module.device_get_sync_jack(self._device)
+        self._verify_error(res)
+        return jack_in == 1, jack_out == 1
+
+    @staticmethod
+    def _verify_error(res):
+        if Result(res) == Result.Failed:
+            raise RuntimeError('Function return status : {}'.format(res))
+
+if __name__ == "__main__":
+    k4a = PyK4A()
+    k4a.device_open()
+    print(k4a.device_get_sync_jack())
+    # k4a.device_close()
