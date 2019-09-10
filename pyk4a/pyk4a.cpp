eb31f8ef6fdd3f7f3b64f62755157f5183c61bc9
Etienne Dubeau <etienne.dubeau.1@ulaval.ca>
2019-09-10 15:10:59 -0400

    Color and depth image working

--- pyk4a/pyk4a.cpp
+++ pyk4a/pyk4a.cpp
@@ -9,28 +9,25 @@ extern "C" {
 #endif
 
     k4a_capture_t capture;
+    k4a_transformation_t transformation_handle;
+    k4a_image_t depth_image;
+    k4a_image_t color_image;
+    k4a_device_t device;
 
     static PyObject* device_open(PyObject* self, PyObject* args){
         int device_id;
         PyArg_ParseTuple(args, "I", &device_id);
-        k4a_device_t device = NULL;
         k4a_result_t result = k4a_device_open(device_id, &device);
 
-        return Py_BuildValue("In", result, device);
+        return Py_BuildValue("I", result);
     }
 
     static PyObject* device_close(PyObject* self, PyObject* args){
-        k4a_device_t device = NULL;
-        PyArg_ParseTuple(args, "n", &device);
-
-        /* k4a_device_close(device); */
-        return NULL;
+        k4a_device_close(device);
+        return Py_BuildValue("I", K4A_RESULT_SUCCEEDED);
     }
 
     static PyObject* device_get_sync_jack(PyObject* self, PyObject* args){
-        k4a_device_t device = NULL;
-        PyArg_ParseTuple(args, "n", &device);
-
         bool in_jack = 0;
         bool out_jack = 0;
         k4a_result_t result = k4a_device_get_sync_jack(device, &in_jack, &out_jack);
@@ -39,44 +36,109 @@ extern "C" {
     }
 
     static PyObject* device_start_cameras(PyObject* self, PyObject* args){
-        k4a_device_t device = NULL;
         k4a_device_configuration_t config = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;
-        PyArg_ParseTuple(args, "nIIIIIIIII", &device, &config.color_format,
+        PyArg_ParseTuple(args, "IIIIIIIII", &config.color_format,
                 &config.color_resolution, &config.depth_mode,
                 &config.camera_fps, &config.synchronized_images_only,
                 &config.depth_delay_off_color_usec, &config.wired_sync_mode,
                 &config.subordinate_delay_off_master_usec,
                 &config.disable_streaming_indicator);
 
-        k4a_result_t result = k4a_device_start_cameras(device, &config);
+        k4a_result_t result;
+        k4a_calibration_t calibration;
+        result = k4a_device_get_calibration(device, config.depth_mode,
+                config.color_resolution, &calibration);
+        if (result == K4A_RESULT_FAILED) {
+            return Py_BuildValue("I", K4A_RESULT_FAILED);
+        }
+        transformation_handle = k4a_transformation_create(&calibration);
+        if (transformation_handle == NULL) {
+            return Py_BuildValue("I", K4A_RESULT_FAILED);
+        }
+        result = k4a_device_start_cameras(device, &config);
         return Py_BuildValue("I", result);
     }
 
-    static PyObject* device_get_capture(PyObject* self, PyObject* args){
-        k4a_device_t device = NULL;
-        int32_t timeout;
-        PyArg_ParseTuple(args, "nI", &device, &timeout);
+    static PyObject* device_stop_cameras(PyObject* self, PyObject* args){
+        if (transformation_handle)
+            k4a_transformation_destroy(transformation_handle);
+        if (capture)
+            k4a_capture_release(capture);
+        if (depth_image)
+            k4a_image_release(depth_image);
+        if (color_image)
+            k4a_image_release(color_image);
+        k4a_device_stop_cameras(device);
+        return Py_BuildValue("I", K4A_RESULT_SUCCEEDED);
+    }
 
+    static PyObject* device_get_capture(PyObject* self, PyObject* args){
+        int32_t timeout;
+        PyArg_ParseTuple(args, "I", &timeout);
+
+        if (capture)
+            k4a_capture_release(capture);
         k4a_capture_create(&capture);
         k4a_wait_result_t result = k4a_device_get_capture(device, &capture, timeout);
         return Py_BuildValue("I", result);
     }
 
     static PyObject* device_get_color_image(PyObject* self, PyObject* args){
-        k4a_image_t color_image = k4a_capture_get_color_image(capture);
-        uint8_t* buffer = k4a_image_get_buffer(color_image);
-        unsigned long dims[2];
-        dims[0] = (unsigned long) k4a_image_get_height_pixels(color_image);
-        dims[1] = (unsigned long) k4a_image_get_width_pixels(color_image);
-        dims[2] = (unsigned long) 4;
-        PyArrayObject* np_color_image = (PyArrayObject*) PyArray_SimpleNewFromData(3, (npy_intp*) dims, NPY_UINT8, buffer);
-        return PyArray_Return(np_color_image);
+        if (color_image)
+            k4a_image_release(color_image);
+        color_image = k4a_capture_get_color_image(capture);
+        if (color_image) {
+            uint8_t* buffer = k4a_image_get_buffer(color_image);
+            unsigned long dims[3];
+            dims[0] = (unsigned long) k4a_image_get_height_pixels(color_image);
+            dims[1] = (unsigned long) k4a_image_get_width_pixels(color_image);
+            dims[2] = (unsigned long) 4;
+            PyArrayObject* np_color_image = (PyArrayObject*) PyArray_SimpleNewFromData(3, (npy_intp*) dims, NPY_UINT8, buffer);
+            return PyArray_Return(np_color_image);
+        }
+        else {
+            return Py_BuildValue("");
+        }
     }
 
-    static PyObject* capture_release(PyObject* self, PyObject* args){
-        k4a_capture_release(capture);
-    }
+    static PyObject* device_get_depth_image(PyObject* self, PyObject* args){
+        int is_transform_enabled;
+        PyArg_ParseTuple(args, "p", &is_transform_enabled);
 
+        if (depth_image)
+            k4a_image_release(depth_image);
+        depth_image = k4a_capture_get_depth_image(capture);
+        if (is_transform_enabled && depth_image && color_image) {
+            k4a_image_t color_image = k4a_capture_get_color_image(capture);
+            k4a_image_t depth_image_transformed;
+            k4a_image_create(
+                    k4a_image_get_format(depth_image),
+                    k4a_image_get_width_pixels(color_image),
+                    k4a_image_get_height_pixels(color_image),
+                    k4a_image_get_width_pixels(color_image) * (int)sizeof(uint16_t),
+                    &depth_image_transformed);
+            k4a_result_t res = k4a_transformation_depth_image_to_color_camera(
+                    transformation_handle,
+                    depth_image, depth_image_transformed);
+            if (res == K4A_RESULT_FAILED)
+                return Py_BuildValue("");
+            k4a_image_release(color_image);
+            k4a_image_release(depth_image);
+            depth_image = depth_image_transformed;
+        }
+
+        if (depth_image) {
+            uint8_t* buffer = k4a_image_get_buffer(depth_image);
+            unsigned long dims[2];
+            dims[0] = (unsigned long) k4a_image_get_height_pixels(depth_image);
+            dims[1] = (unsigned long) k4a_image_get_width_pixels(depth_image);
+            PyArrayObject* np_depth_image = (PyArrayObject*) PyArray_SimpleNewFromData(2, (npy_intp*) dims, NPY_UINT16, buffer);
+            return PyArray_Return(np_depth_image);
+        }
+        else {
+            return Py_BuildValue("");
+        }
+    }
 
     // Source : https://github.com/MathGaron/pyvicon/blob/master/pyvicon/pyvicon.cpp
     //###################
@@ -97,9 +159,10 @@ extern "C" {
     static PyMethodDef Pyk4aMethods[] = {
         {"device_open", device_open, METH_VARARGS, "Open an Azure Kinect device"},
         {"device_start_cameras", device_start_cameras, METH_VARARGS, "Starts color and depth camera capture"},
+        {"device_stop_cameras", device_stop_cameras, METH_VARARGS, "Stops the color and depth camera capture"},
         {"device_get_capture", device_get_capture, METH_VARARGS, "Reads a sensor capture"},
-        {"capture_release", capture_release, METH_VARARGS, "Release a capture"},
         {"device_get_color_image", device_get_color_image, METH_VARARGS, "Get the color image associated with the given capture"},
+        {"device_get_depth_image", device_get_depth_image, METH_VARARGS, "Set or add a depth image to the associated capture"},
         {"device_close", device_close, METH_VARARGS, "Close an Azure Kinect device"},
         {"device_get_sync_jack", device_get_sync_jack, METH_VARARGS, "Get the device jack status for the synchronization in and synchronization out connectors."},
         {NULL, NULL, 0, NULL}
--- pyk4a/pyk4a.py
+++ pyk4a/pyk4a.py
@@ -10,28 +10,49 @@ class Result(Enum):
 
 class PyK4A:
     TIMEOUT_WAIT_INFINITE = -1
+    def __init__(self, config : Config, device_id=0):
+        self._device_id = 0
+        self._config = config
 
-    def device_open(self, device_id=0):
-        res, self._device = k4a_module.device_open(device_id)
+    def connect(self):
+        self._device_open()
+        self._device_start_cameras()
+
+    def disconnect(self):
+        self._device_stop_cameras()
+        self._device_close()
+
+    def _device_open(self):
+        res = k4a_module.device_open(self._device_id)
         self._verify_error(res)
 
-    def device_close(self):
-        k4a_module.device_close(self._device)
-
-    def device_start_cameras(self, config : Config):
-        res = k4a_module.device_start_cameras(self._device, *config.unpack())
+    def _device_close(self):
+        res = k4a_module.device_close()
         self._verify_error(res)
 
-    def device_get_capture(self, timeout = TIMEOUT_WAIT_INFINITE):
-        res = k4a_module.device_get_capture(self._device, timeout)
+    def _device_start_cameras(self):
+        res = k4a_module.device_start_cameras(*self._config.unpack())
+        self._verify_error(res)
+
+    def _device_stop_cameras(self):
+        res = k4a_module.device_stop_cameras()
+        self._verify_error(res)
+
+    def device_get_capture(self, timeout=TIMEOUT_WAIT_INFINITE,
+            color_only=False, transform_depth_to_color=True):
+        res = k4a_module.device_get_capture(timeout)
         self._verify_error(res)
 
         color = k4a_module.device_get_color_image()
-        return color
-        # depth = k4a_module.device_get_depth()
+        if color_only:
+            return color
+
+        depth = k4a_module.device_get_depth_image(transform_depth_to_color)
+
+        return color, depth
 
     def device_get_sync_jack(self):
-        res, jack_in, jack_out = k4a_module.device_get_sync_jack(self._device)
+        res, jack_in, jack_out = k4a_module.device_get_sync_jack()
         self._verify_error(res)
         return jack_in == 1, jack_out == 1
 
@@ -41,12 +62,13 @@ class PyK4A:
             raise RuntimeError('Function return status : {}'.format(res))
 
 if __name__ == "__main__":
-    k4a = PyK4A()
-    k4a.device_open()
-    print(k4a.device_get_sync_jack())
-    print(k4a.device_start_cameras(Config()))
-    from matplotlib import pyplot as plt
-    for _ in range(100):
-        plt.imshow(k4a.device_get_capture())
-        plt.show()
-    # k4a.device_close()
+    k4a = PyK4A(Config())
+    k4a.connect()
+    print("Connected")
+    jack_in, jack_out = k4a.device_get_sync_jack()
+    print("Jack status : in -> {} , out -> {}".format(jack_in, jack_out))
+    for _ in range(10):
+        color, depth = k4a.device_get_capture(color_only=False)
+        print(color.shape, depth.shape)
+    k4a.disconnect()
+    print("Disconnected")
