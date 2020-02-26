0198131ea908be8731a6c515318d0a1dc10c1b8c
Etienne Dubeau <etienne.dubeau.1@ulaval.ca>
2020-02-26 09:26:40 -0500

    Add method to get IR capture

--- pyk4a/pyk4a.py
+++ pyk4a/pyk4a.py
@@ -104,6 +104,9 @@ class PyK4A:
     def _get_capture_color(self) -> Optional[np.ndarray]:
         return k4a_module.device_get_color_image()
 
+    def _get_capture_ir(self) -> Optional[np.ndarray]:
+        return k4a_module.device_get_ir_image()
+
     def _get_capture_depth(self, transform_depth_to_color: bool) -> Optional[np.ndarray]:
         depth = k4a_module.device_get_depth_image()
         if transform_depth_to_color:
