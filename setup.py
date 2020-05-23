887d89bedf1854a89d31ad4932399ca7c6f2393b
Louis-Philippe Asselin <lpasselin@users.noreply.github.com>
2020-05-23 13:57:19 -0400

    add python version requirement (#26)
    * added python version requirement
    
    * block python version 2 which cannot use python_requires

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, Extension
 
+import sys
+if sys.version_info[0] == 2:
+    sys.exit("Python 2 is not supported.")
+
 
 # Bypass import numpy before running install_requires
 # https://stackoverflow.com/questions/54117786/add-numpy-get-include-argument-to-setuptools-without-preinstalled-numpy
@@ -20,6 +24,7 @@ setup(name='pyk4a',
       license='GPL-3.0',
       author='Etienne Dubeau',
       install_requires=['numpy'],
+      python_requires='>=3.4',
       author_email='etienne.dubeau.1@ulaval.ca',
       url='https://github.com/etiennedub/pyk4a/',
       download_url='https://github.com/etiennedub/pyk4a/archive/0.2.tar.gz',
