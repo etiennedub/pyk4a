4c256499f523c09c94b684f8d874aacc88da33e4
Louis-Philippe Asselin <louisphilippeasselin@gmail.com>
2020-09-03 15:45:18 -0400

    fmt body tracking old examples

--- example/pose_display_2d.py
+++ example/pose_display_2d.py
@@ -1,14 +1,15 @@
-import numpy as np
 import cv2
-import pyk4a
-from pyk4a import Config, PyK4A, ColorResolution
+import numpy as np
 
-#mapping from kinect's pose to openpose's coco pose
+import pyk4a
+from pyk4a import ColorResolution, Config, PyK4A
+
+
+# mapping from kinect's pose to openpose's coco pose
 kinect2coco = [27, 3, 12, 13, 14, 5, 6, 7, 22, 23, 24, 18, 19, 20, 30, 28, 31, 29]
 num_joints = len(kinect2coco)
 
-k4a = PyK4A(Config(color_resolution=ColorResolution.RES_720P,
-                   depth_mode=pyk4a.DepthMode.NFOV_UNBINNED))
+k4a = PyK4A(Config(color_resolution=ColorResolution.RES_720P, depth_mode=pyk4a.DepthMode.NFOV_UNBINNED))
 k4a.connect()
 
 while True:
@@ -20,14 +21,14 @@ while True:
 
     if pose is not None and pose.shape[0] > 0:
         pts = pose[0, :, :2].reshape(-1, 2)[kinect2coco]
-        pts[:, 0] = pts[:, 0]*(target_H/source_H)
-        pts[:, 1] = pts[:, 1]*(target_W/source_W)
+        pts[:, 0] = pts[:, 0] * (target_H / source_H)
+        pts[:, 1] = pts[:, 1] * (target_W / source_W)
 
         for i in range(pts.shape[0]):
             cv2.circle(frame, (int(pts[i, 0]), int(pts[i, 1])), 15, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
 
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow("frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 k4a.disconnect()
--- example/pose_display_3d.py
+++ example/pose_display_3d.py
@@ -1,28 +1,29 @@
-from pyqtgraph.Qt import QtCore, QtGui
-import pyqtgraph.opengl as gl
-import pyqtgraph as pg
-import numpy as np
 import sys
 
-import pyk4a
-from pyk4a import Config, PyK4A, ColorResolution
+import numpy as np
+import pyqtgraph as pg
+import pyqtgraph.opengl as gl
+from pyqtgraph.Qt import QtCore, QtGui
 
-#mapping from kinect's pose to openpose's coco pose
+import pyk4a
+from pyk4a import ColorResolution, Config, PyK4A
+
+
+# mapping from kinect's pose to openpose's coco pose
 kinect2coco = [27, 3, 12, 13, 14, 5, 6, 7, 22, 23, 24, 18, 19, 20, 30, 28, 31, 29]
 num_joints = len(kinect2coco)
 
-#https://gist.github.com/markjay4k/da2f55e28514be7160a7c5fbf95bd243
+# https://gist.github.com/markjay4k/da2f55e28514be7160a7c5fbf95bd243
 class Visualizer(object):
     def __init__(self):
-        self.k4a = PyK4A(Config(color_resolution=ColorResolution.RES_720P,
-                           depth_mode=pyk4a.DepthMode.NFOV_UNBINNED, ))
+        self.k4a = PyK4A(Config(color_resolution=ColorResolution.RES_720P, depth_mode=pyk4a.DepthMode.NFOV_UNBINNED,))
         self.k4a.connect()
 
         self.traces = dict()
         self.app = QtGui.QApplication(sys.argv)
         self.w = gl.GLViewWidget()
-        self.w.opts['distance'] = 15000
-        self.w.setWindowTitle('Pose')
+        self.w.opts["distance"] = 15000
+        self.w.setWindowTitle("Pose")
         self.w.setGeometry(0, 110, 1920, 1080)
         self.w.show()
 
@@ -37,12 +38,11 @@ class Visualizer(object):
         self.n = 10
         for i in range(self.n):
             pts = np.zeros((num_joints, 3))
-            self.traces[i] = gl.GLScatterPlotItem(pos=pts,
-                                                  color=pg.glColor((i, self.n * 1.3)))
+            self.traces[i] = gl.GLScatterPlotItem(pos=pts, color=pg.glColor((i, self.n * 1.3)))
             self.w.addItem(self.traces[i])
 
     def start(self):
-        if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        if (sys.flags.interactive != 1) or not hasattr(QtCore, "PYQT_VERSION"):
             QtGui.QApplication.instance().exec_()
 
     def set_plotdata(self, name, points, color):
@@ -54,13 +54,9 @@ class Visualizer(object):
         if pose is not None:
             for i in range(pose.shape[0]):
                 pts = pose[i, :, :3].reshape(-1, 3)[kinect2coco]
-                self.set_plotdata(name=i,
-                                  points=pts,
-                                  color=pg.glColor((i, self.n * 0.3)))
+                self.set_plotdata(name=i, points=pts, color=pg.glColor((i, self.n * 0.3)))
             for i in range(pose.shape[0], self.n):
-                self.set_plotdata(name=i,
-                                  points=np.zeros((1, 3)),
-                                  color=pg.glColor((i, self.n * 0.3)))
+                self.set_plotdata(name=i, points=np.zeros((1, 3)), color=pg.glColor((i, self.n * 0.3)))
 
     def animation(self):
         timer = QtCore.QTimer()
@@ -70,6 +66,6 @@ class Visualizer(object):
 
 
 # Start Qt event loop unless running in interactive mode.
-if __name__ == '__main__':
+if __name__ == "__main__":
     v = Visualizer()
     v.animation()
