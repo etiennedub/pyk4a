45fae214a76929b36c689fef050d88fe4473e582
Ilya Gruzinov <igruzinov@yandex.ru>
2020-09-18 18:33:48 +0300

    Remove PytestAssertRewriteWarning

--- tests/plugins/capture.py
+++ tests/plugins/capture.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
-from typing import Optional
+from typing import Callable, Optional
 
 import numpy as np
+import pytest
 
 
 @dataclass(frozen=True)
@@ -11,15 +12,19 @@ class CaptureHandle:
     ir: Optional[np.ndarray]
 
 
-def random_capture(color: bool = True, depth: bool = True, ir: bool = True) -> CaptureHandle:
-    color_image: Optional[np.ndarray] = None
-    depth_image: Optional[np.ndarray] = None
-    ir_image: Optional[np.ndarray] = None
-    if color:
-        color_image = np.random.randint(low=0, high=255, size=(720, 1280, 4), dtype=np.uint8)
-    if depth:
-        depth_image = np.random.randint(low=0, high=15000, size=(576, 640), dtype=np.uint16)
-    if ir:
-        color_image = np.random.randint(low=0, high=3000, size=(576, 640), dtype=np.uint16)
+@pytest.fixture()
+def capture_factory() -> Callable:
+    def random_capture(color: bool = True, depth: bool = True, ir: bool = True) -> CaptureHandle:
+        color_image: Optional[np.ndarray] = None
+        depth_image: Optional[np.ndarray] = None
+        ir_image: Optional[np.ndarray] = None
+        if color:
+            color_image = np.random.randint(low=0, high=255, size=(720, 1280, 4), dtype=np.uint8)
+        if depth:
+            depth_image = np.random.randint(low=0, high=15000, size=(576, 640), dtype=np.uint16)
+        if ir:
+            color_image = np.random.randint(low=0, high=3000, size=(576, 640), dtype=np.uint16)
 
-    return CaptureHandle(color=color_image, depth=depth_image, ir=ir_image)
+        return CaptureHandle(color=color_image, depth=depth_image, ir=ir_image)
+
+    return random_capture
