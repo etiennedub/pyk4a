4e4248f234138028bb458b3d53a1dddbb85da28a
Louis-Philippe Asselin <louisphilippeasselin@gmail.com>
2021-06-28 14:43:47 -0400

    fix multiline string

--- pyk4a/module.py
+++ pyk4a/module.py
@@ -12,15 +12,20 @@ except BaseException as e:
             import k4a_module  # noqa: F401
         except BaseException:
             raise ImportError(
-                f"""Cannot import k4a_module.
-                DLL directory added was {added_dll_dir}.
-                You can provide a different path containing `k4a.dll` using the environment variable `K4A_DLL_DIR`.
-                Also make sure pyk4a was properly built."""
+                (
+                    f"Cannot import k4a_module. "
+                    "DLL directory added was {added_dll_dir}. "
+                    "You can provide a different path containing `k4a.dll`"
+                    "using the environment variable `K4A_DLL_DIR`. "
+                    "Also make sure pyk4a was properly built."
+                )
             ) from e
     else:
         raise ImportError(
-            """Cannot import k4a_module.
-            Make sure `libk4a.so` can be found.
-            Add the directory to your `LD_LIBRARY_PATH` if required.
-            Also make sure pyk4a is properly built."""
+            (
+                "Cannot import k4a_module. "
+                "Make sure `libk4a.so` can be found. "
+                "Add the directory to your `LD_LIBRARY_PATH` if required. "
+                "Also make sure pyk4a is properly built."
+            )
         ) from e
