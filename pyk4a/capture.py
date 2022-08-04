50a5e032308f0119a00087a3128662f1889ef264
Justin <lego.man.123@hotmail.com>
2022-08-04 08:51:28 -0600

    Color image ISO speed for Capture (#176)
    * Color image ISO speed for Capture
    
    * version bump
    
    * add linux installation info for .dev package
    
    Co-authored-by: Louis-Philippe Asselin <louisphilippeasselin@gmail.com>

--- pyk4a/capture.py
+++ pyk4a/capture.py
@@ -27,6 +27,7 @@ class PyK4ACapture:
         self._color_timestamp_usec: int = 0
         self._color_system_timestamp_nsec: int = 0
         self._color_exposure_usec: Optional[int] = None
+        self._color_iso_speed: Optional[int] = None
         self._color_white_balance: Optional[int] = None
         self._depth: Optional[np.ndarray] = None
         self._depth_timestamp_usec: int = 0
@@ -72,6 +73,15 @@ class PyK4ACapture:
                 raise K4AException("Cannot read exposure from color image")
             self._color_exposure_usec = value
         return self._color_exposure_usec
+
+    @property
+    def color_iso_speed(self) -> int:
+        if self._color_iso_speed is None:
+            value = k4a_module.color_image_get_iso_speed(self._capture_handle)
+            if value == 0:
+                raise K4AException("Cannot read iso from color_image")
+            self._color_iso_speed = value
+        return self._color_iso_speed
 
     @property
     def color_white_balance(self) -> int:
